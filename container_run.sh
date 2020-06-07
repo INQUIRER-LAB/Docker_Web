2ff054a363d32d0789ec6f6653c33980db6a92ec
INQUIRER-LAB <tayez788@gmail.com>
2020-06-08 02:32:58 +0900

    update contener_run

--- container_run.sh
+++ container_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 read -p "container_name:" container_name  
-read -p "PORT:" port
-docker container run -itd --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro --stop-signal SIGRTMIN+3  --name $container_name -h $container_name -p $port
+read -p "PORT1:" port
+read -p "PORT2:" port2
+docker container run -itd --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro --stop-signal SIGRTMIN+3  --name $container_name -h $container_name -p $port -p $port2 centos:centos7 /sbin/init
